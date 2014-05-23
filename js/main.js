078269a2be20492f80c66f0418c52662c77ee4ba
Roland Grenke <roland.grenke@starlize.me>
2014-05-23 09:57:58 +0200

    added new sound files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON('../nippel/assets.json', function(data) {
+    $.getJSON('assets.json', function(data) {
         $.each(data.nipples, function(k, nipple) {
             var html = '<div class="nipple"><audio><source src="' + nipple.file + '" type="audio/mpeg"></audio><button class="btn btn-default">' + nipple.name + '</button></div>';
 
